c4010961d3b4f5405eb4dd5972c1ad81c5d46478
Eidonium <68496541+Eidonium@users.noreply.github.com>
2021-09-22 08:47:08 +0700

    Add files via upload

---
+++ js/testimonial.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,3],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,2],
+        itemsMobile:[650,1],
+        pagination:true,
+        autoPlay:true
+    });
+});
